e089be296c02921b0f901a757ed7ea9969f4a6d1
Nyrk19 <127149633+Nyrk19@users.noreply.github.com>
2023-04-20 08:51:47 -0600

    Update Serv_P2.py

--- Serv_P2.py
+++ Serv_P2.py
@@ -1,28 +1,31 @@
-import sys
 import socket
 import selectors
-import types
-import logging
-sel = selectors.DefaultSelector()
+import os
 
-def accept(sock_a, mask):
-    sock_conn, addr = sock_a.accept()  # Should be ready
+sel = selectors.DefaultSelector() #Creamos una instancia de DefaultSelector, que proporciona una interfaz para esperar eventos en sockets.
+conn = {} #Diccionario vacío que se utiliza para almacenar la información de conexión de los sockets que se vayan conectando
+
+def accept(sock_a, mask): #La función accept() se utiliza como un callback cuando el socket del servidor está listo para aceptar una nueva conexión.
+    sock_conn, addr = sock_a.accept() #Aceptamos la conexion entrante, guardamos los datos del socket y la dirección
     print('aceptado', sock_conn, ' de', addr)
-    sock_conn.setblocking(False)
+    sock_conn.setblocking(False) #Indicamos que sera un socket no bloqueante
     sel.register(sock_conn, selectors.EVENT_READ | selectors.EVENT_WRITE, read_write)
+    conn[sock_conn] = os.path.join(os.getcwd(),f"audioRec_{addr[0]}_{addr[1]}.mp3") #Se guarda la información de conexión del socket en el diccionario conn
 
-def read_write(sock_c, mask):
+
+def read_write(sock_c, mask): #La función que se llama cada vez que se detecta un evento de lectura o escritura
     if mask & selectors.EVENT_READ:
-        data = sock_c.recv(1024)  # Should be ready
+        data = sock_c.recv(1024)
         if data:
-            print('recibido', repr(data), 'a', sock_c)
+            print('recibido', sock_c)
             # Abrir archivo en modo append y escribir los datos recibidos
-            with open("audio_received.mp3", "ab") as f:
+            with open(conn[sock_c], "ab") as f:
                 f.write(data)
         else:
             print('cerrando', sock_c)
             sel.unregister(sock_c)
             sock_c.close()
+            del conn[sock_c]
     if mask & selectors.EVENT_WRITE:
        print ("")
 
