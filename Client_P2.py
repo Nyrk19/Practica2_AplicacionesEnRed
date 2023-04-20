f21d9fc4c27813c5f564aeb610375ebe10d1dc53
Nyrk19 <127149633+Nyrk19@users.noreply.github.com>
2023-04-20 08:53:09 -0600

    Update Client_P2.py

--- Client_P2.py
+++ Client_P2.py
@@ -1,15 +1,13 @@
 import socket
 
-def send_audio_file(filename, server_address, server_port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((server_address, server_port))
-        with open(filename, "rb") as f:
-            data = f.read()
-            sock.sendall(data)
-        print("Archivo de audio enviado con éxito.")
-
-if __name__ == "__main__":
-    filename = "audio.mp3" # nombre del archivo de audio que se quiere enviar
-    server_address = "localhost" # dirección IP o nombre de dominio del servidor
-    server_port = 12345 # puerto del servidor
-    send_audio_file(filename, server_address, server_port)
+server_address = "localhost"  # dirección IP o nombre de dominio del servidor
+server_port = 12345  # puerto del servidor
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: #Creamos el socket
+    sock.connect((server_address, server_port)) #Establece una conexión con el servidor
+    filename = input("Ingrese el nombre del archivo: ")  # nombre del archivo de audio que se quiere enviar
+    """Abre el archivo de audio especificado por filename en modo de lectura binaria ("rb") lee todo su contenido en
+        una variable llamada data, y luego envía todo el contenido del archivo al servidorutilizando el método sendall()."""
+    with open(filename, "rb") as f:
+        data = f.read()
+        sock.sendall(data)
+    print("Archivo de audio enviado con éxito.")
